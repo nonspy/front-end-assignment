761a2b181261d8a2c52430b7779523db03cc28bb
Max Mind <nonspy.eq@gmail.com>
2022-02-15 23:47:27 +0200

    Task 1: bacon multiplication and styling

--- frontend/src/components/Bacon/Bacon.tsx
+++ frontend/src/components/Bacon/Bacon.tsx
@@ -1,7 +1,25 @@
-import React from "react"
+import React, { useState } from "react"
 
 const Bacon: React.FC = () => {
+  const [count, setCount] = useState<number>(1)
+  const image = "/images/bacon.jpg"
 
+  const handleClick = () => {
+    setCount(prev => prev + 1)
+  }
+
+  return (
+    <section>
+      <button type="button" onClick={handleClick}>
+        Yeah, I want more bacon!
+      </button>
+      <ul>
+        {Array.from(Array(count).keys()).map(() => (
+          <li><img src={image} alt="Beautiful slices of fried bacon" /></li>
+        ))}
+      </ul>
+    </section>
+  )
 }
 
 export default Bacon
